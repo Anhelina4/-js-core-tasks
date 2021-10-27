b566c5019a1576095db16cffc683480d8cb47fc0
Anhelina4 <bogdanova_angelina@ukr.net>
2021-10-27 14:53:40 +0300

    4-task not finished

---
+++ 4-task.js
@@ -0,0 +1,39 @@
+"use strict"
+
+function sequence(start=0, step=1) {
+    let number=start
+  return function() { 
+    let returnNumber = number
+    number+=step
+    start=number
+    return returnNumber;
+  };
+}
+
+function fmap(a, gen, ){
+  return function (){
+      return a(gen() )
+      }
+      
+}
+
+
+  function add(a, b) { 
+    return a + b; 
+}
+
+function square(x) { return x * x; }
+
+var gen = sequence(1, 1);
+
+var squareGen = fmap(square, gen);
+
+console.log(squareGen()); // 1
+console.log(squareGen()); // 4
+console.log(squareGen()); // 9
+console.log(squareGen()); // 16
+
+// Мы получаем новую функцию, которая вызвает add, и результат пропускает через функцию square
+var squareAdd = fmap(square, add);
+console.log(squareAdd(2, 3)); // 25 = (2 + 3) ^ 2
+console.log(squareAdd(5, 7)); // 144 = (5 + 7) ^ 2
