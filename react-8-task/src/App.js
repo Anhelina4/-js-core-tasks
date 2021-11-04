ecfc7b2f3060254eb7000414b40ce8dbce912a62
Anhelina4 <bogdanova_angelina@ukr.net>
2021-11-04 16:07:13 +0200

    react-8-task

---
+++ react-8-task/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './App.css';
+import Pagination from  "./Pagination"
+function App() {
+  return (
+    <div className="App">
+      <Pagination material="101" limit="5"/>
+    </div>
+  );
+}
+
+export default App;
