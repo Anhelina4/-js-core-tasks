944000059e34733901eba4b96721f4aab3629298
Anhelina4 <bogdanova_angelina@ukr.net>
2021-11-05 00:04:25 +0200

    react-state-3-task

--- react-state-1-task/src/App.js
+++ react-state-1-task/src/App.js
@@ -2,11 +2,14 @@
 import './App.css';
 import Component from "./Component/Component"
 import { TextHidden } from './TextHidden';
+import Container from "./Slider/Container"
 function App() {
   return (
     <div className="App">
       {/* <Component/> */}
-      <TextHidden/>
+      {/* <TextHidden/> */}
+      <Container/>
+
     </div>
   );
 }
